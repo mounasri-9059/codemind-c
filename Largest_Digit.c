80de207c4ecf2fa7595b7992837bdcdcf1999043
mounasri-9059 <119719503+mounasri-9059@users.noreply.github.com>
2022-12-21 20:34:42 +0530

    #include<stdio.h> int main() {     int n,i,r,s=0,max=-999;     scanf("%d",&n);     while(n!=0)     {         r=n%10;         n=n/10;     if(r>max)         {             max=r;         }     }     printf("%d",max); }

---
+++ Largest_Digit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,r,s=0,max=-999;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        r=n%10;
+        n=n/10;
+    if(r>max)
+        {
+            max=r;
+        }
+    }
+    printf("%d",max);
+}
+
